7679e509d28053f282fdfa3ee24fce7dda3c9d3b
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-08 23:17:53 +0100

    Moved to maven

--- src/test/java/com/github/lands/PickleSerializationTest.java
+++ src/test/java/com/github/lands/PickleSerializationTest.java
@@ -11,20 +11,20 @@ public class PickleSerializationTest {
 
     @Test
     public void canLoadWorldWithoutErrors() throws IOException {
-        File file = new File("examples/seed_77.world");
+        File file = new File("examples-worlds/seed_77.world");
         World world = PickleSerialization.loadWorld(file);
     }
 
     @Test
     public void nameIsLoaded() throws IOException {
-        File file = new File("examples/seed_77.world");
+        File file = new File("examples-worlds/seed_77.world");
         World world = PickleSerialization.loadWorld(file);
         assertEquals("seed_77", world.getName());
     }
 
     @Test
     public void dimensionIsLoaded() throws IOException {
-        File file = new File("examples/seed_77.world");
+        File file = new File("examples-worlds/seed_77.world");
         World world = PickleSerialization.loadWorld(file);
         assertEquals(new Dimension(512, 512), world.getDimension());
     }
