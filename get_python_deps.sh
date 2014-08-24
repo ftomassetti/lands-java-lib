7b35d9e2c0da1fa6f49df8cc59a3541dc46586f4
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-24 13:38:16 +0100

    Updating python deps when building

--- get_python_deps.sh
+++
@@ -1,2 +0,0 @@
-git clone git@github.com:ftomassetti/langgen.git python/langgen
-git clone git@github.com:ftomassetti/lands.git python/lands
---
+++ update_python_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Updating python deps..."
+git submodule update --init --recursive
+echo "...python deps updated"
